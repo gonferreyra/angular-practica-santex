e298131fd622dee3be90e825f6b1337936a11401
Gonzalo Ferreyra <gonferreyra@gmail.com>
2023-07-12 12:51:38 -0300

    form is send to another component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { ListHomeComponent } from './list/pages/list-home.component';
+import { AddFormComponent } from './list/components/add-form/add-form.component';
 
 @NgModule({
-  declarations: [AppComponent, ListHomeComponent],
+  declarations: [AppComponent, ListHomeComponent, AddFormComponent],
   imports: [BrowserModule, MatButtonModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/list/components/add-form/add-form.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ListService } from '../../services/list.service';
+
+@Component({
+  selector: 'list-add-form',
+  templateUrl: './add-form.component.html',
+  styleUrls: ['./add-form.component.scss'],
+})
+export class AddFormComponent {
+  public newItemForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: ['', [Validators.required, Validators.minLength(2)]],
+  });
+
+  constructor(private fb: FormBuilder, private listService: ListService) {}
+
+  // Validaciones de errores
+  isNotValidField(field: string): boolean | null {
+    return (
+      this.newItemForm.controls[field].errors &&
+      this.newItemForm.controls[field].touched
+    );
+  }
+
+  onSubmit() {
+    if (this.newItemForm.invalid) {
+      this.newItemForm.markAllAsTouched();
+      return;
+    }
+
+    this.listService.list.push(this.newItemForm.value);
+    this.listService.saveToLocalStorage();
+    this.newItemForm.reset();
+  }
+}
